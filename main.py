4bb3b1047cedad80c998d094c77e197e0134d444
theSm1th <68665004+theSm1th@users.noreply.github.com>
2023-03-31 16:34:33 +0100

    Added some more stats - data parsing into standard formats is still WIP due to a dumb issue I'm having.

--- main.py
+++ main.py
@@ -12,19 +12,32 @@ def format_temp(to_format):
     return "{:.1f}*".format(to_format)
 
 
+def remove_percent_space(dataIn):
+    dataIn = dataIn.replace("%", "").replace(" ", "")
+    print(dataIn)
+    return int(dataIn)
+
+
 def comp_get_stats():
-    data_get = requests.get("http://localhost:8085/data.json")
-    json_data = json.loads(data_get.content)
+    # data_get = requests.get("http://localhost:8085/data.json")
+    # json_data = json.loads(data_get.content)
+    with open("data.json", "r") as file:
+        json_data = json.loads(file.readlines()[0])
 
     cpu_data = json_data["Children"][0]["Children"][1]["Children"]
     cpu_temp = cpu_data[3]["Children"][0]["Value"]
     cpu_util = cpu_data[4]["Children"][0]["Value"]
 
     gpu_data = json_data["Children"][0]["Children"][3]["Children"]
-    gpu_temp = json_data
-    gpu_util = gpu_data # TODO: Finish this on computer - Xe does not show aggregated util
-    return {"cpu_temp":cpu_temp, "cpu_util":cpu_util, "gpu_temp":gpu_temp, "gpu_util": gpu_util}
-    #return json.dumps(cpu_data[4]["Children"][0]["Value"], indent=4)
+    gpu_temp = gpu_data[3]["Children"][0]["Value"]
+    gpu_util = gpu_data[4]["Children"][0]["Value"]
+
+    fan_data = json_data["Children"][0]["Children"][0]["Children"][0]["Children"][3]["Children"]
+    avg_fan_spd = (remove_percent_space(fan_data[0]["Value"]) + remove_percent_space(fan_data[1]["Value"]) + remove_percent_space(fan_data[2]["Value"])) / 3
+    # return {"cpu_temp": cpu_temp, "cpu_util": cpu_util, "gpu_temp": gpu_temp, "gpu_util": gpu_util}
+    print("fan speed: " + avg_fan_spd)
+    return json.dumps(gpu_data[4]["Children"][0]["Value"], indent=4)
+
 
 # TODO: Add functions to strip spaces from inputs from lhm, also add leading 0s/spaces to fill out 20 chars per line
 #  for the display
