1e81ec97a6a281f446b53c234f9ebe1f295aef39
theSm1th <68665004+theSm1th@users.noreply.github.com>
2023-04-01 16:06:13 +0100

    Did everything else

--- main.py
+++ main.py
@@ -1,49 +1,52 @@
 import serial
 import requests
 import json
+import time
+
+ser = serial.Serial("COM9", 9600, timeout=0.05)
 
 
-# ser = serial.Serial("COM9", 9600, timeout=0.05)
-# ser.write(b"WINGMAN             00.0* CPU  GPU 00.0*100%  1000 RPM  100%####################")
-
-def format_temp(to_format):
-    if to_format >= 100:
-        return "OVERL"
-    return "{:.1f}*".format(to_format)
-
-
-def remove_percent_space(dataIn):
-    dataIn = dataIn.replace("%", "").replace(" ", "")
-    print(dataIn)
-    return int(dataIn)
+def lead_spacing(value, desired_length, to_replace):
+    value = str(value)
+    if len(value) != desired_length:
+        to_add = desired_length - len(value)
+        return to_add * to_replace + value
+    else:
+        return value
 
 
 def comp_get_stats():
-    # data_get = requests.get("http://localhost:8085/data.json")
-    # json_data = json.loads(data_get.content)
-    with open("data.json", "r") as file:
-        json_data = json.loads(file.readlines()[0])
+    data_get = requests.get("http://localhost:8085/data.json")
+    json_data = json.loads(data_get.content)
 
     cpu_data = json_data["Children"][0]["Children"][1]["Children"]
-    cpu_temp = cpu_data[3]["Children"][0]["Value"]
-    cpu_util = cpu_data[4]["Children"][0]["Value"]
+    cpu_temp = int(cpu_data[3]["Children"][0]["Value"][:-5])
+    cpu_util = int(cpu_data[4]["Children"][0]["Value"][:-4])
 
     gpu_data = json_data["Children"][0]["Children"][3]["Children"]
-    gpu_temp = gpu_data[3]["Children"][0]["Value"]
-    gpu_util = gpu_data[4]["Children"][0]["Value"]
+    gpu_temp = int(gpu_data[3]["Children"][0]["Value"][:-5])
+    gpu_util = int(gpu_data[4]["Children"][0]["Value"][:-4])
 
-    fan_data = json_data["Children"][0]["Children"][0]["Children"][0]["Children"][3]["Children"]
-    avg_fan_spd = (remove_percent_space(fan_data[0]["Value"]) + remove_percent_space(fan_data[1]["Value"]) + remove_percent_space(fan_data[2]["Value"])) / 3
-    # return {"cpu_temp": cpu_temp, "cpu_util": cpu_util, "gpu_temp": gpu_temp, "gpu_util": gpu_util}
-    print("fan speed: " + avg_fan_spd)
-    return json.dumps(gpu_data[4]["Children"][0]["Value"], indent=4)
+    fan_data = json_data["Children"][0]["Children"][0]["Children"][0]["Children"][2]["Children"]
+    avg_fan_spd = int((int(fan_data[0]["Value"][:-4]) +
+                       int(fan_data[1]["Value"][:-4]) +
+                       int(fan_data[2]["Value"][:-4])) / 3)
 
-
-# TODO: Add functions to strip spaces from inputs from lhm, also add leading 0s/spaces to fill out 20 chars per line
-#  for the display
-# sp_util_fan = f"{}%  {} RPM  {}%"
-# ser.write(b"{sp_status}{}* CPU  GPU {}*{}")
+    return {"cpu_temp": cpu_temp, "cpu_util": cpu_util,
+            "gpu_temp": gpu_temp, "gpu_util": gpu_util,
+            "avg_fan_spd": avg_fan_spd}
 
 
 if __name__ == "__main__":
-    print(comp_get_stats())
+    data = comp_get_stats()
+    print(data)
+    header = "WINGMAN             "
+    sp_util_fan = "{}%  {} RPM  {}%".format(lead_spacing(data["cpu_util"], 3, " "),
+                                            lead_spacing(data["avg_fan_spd"], 4, " "),
+                                            lead_spacing(data["gpu_util"], 3, " "))
+
+    sp_temps = "{}C CPU    GPU {}C".format(lead_spacing(data["cpu_temp"], 3, " "),
+                                           lead_spacing(data["gpu_temp"], 3, " "))
+    flavour_text = "Blast Processing..."
+    string_to_write = f"{header}{sp_util_fan}{sp_temps}{flavour_text}"
+    ser.write(string_to_write.encode())
